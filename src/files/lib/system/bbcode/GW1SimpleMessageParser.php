0d56071f13f91d88462858797df649a99de92955
codemasher <smiley@chillerlan.net>
2018-05-19 20:17:35 +0200

    :octocat:

---
+++ src/files/lib/system/bbcode/GW1SimpleMessageParser.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ *
+ * @filesource   GW1SimpleMessageParser.php
+ * @created      19.05.2018
+ * @author       smiley <smiley@chillerlan.net>
+ * @copyright    2018 smiley
+ * @license      MIT
+ */
+
+namespace wcf\system\bbcode;
+
+use chillerlan\BBCode\BBCode;
+use chillerlan\GW1DB\GW1DBOptions;
+use chillerlan\GW1DB\GWBBCode\GWBBMiddleware;
+use chillerlan\GW1DB\GWBBCode\GWBBOutput;
+
+
+class GW1SimpleMessageParser extends SimpleMessageParser {
+
+
+	public function parse($message, $parseURLs = true, $parseSmilies = true) {
+
+		$gwbboptions = new GW1DBOptions([
+			'outputInterface'           => GWBBOutput::class,
+				'parserMiddlewareInterface' => GWBBMiddleware::class,
+				'sanitizeInput'             => true,
+				'preParse'                  => true,
+				'postParse'                 => true,
+				'language'                  => '[LANG FROM WCF OPTIONS]',
+				'gwdbURL'                   => 'https://smiley.codes/gwdb', // @todo: replace with your own
+			]);
+
+		return (new BBCode($gwbboptions))->parse(parent::parse($message, $parseURLs, $parseSmilies));
+	}
+
+}
+
+
